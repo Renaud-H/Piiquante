325e1a75f6efd73a1f9fa6f976912d235a354586
Renaud-H <theqof@hotmail.fr>
2023-09-22 21:12:26 +0200

    contrôle d'utilisateur

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,19 +1,31 @@
-const bcrypt = require('bcrypt');
+// Import des modules
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+// const
 
-const User = require('../models/user');
-
-const jwt = require('jsonwebtoken');
-
+// Inscription
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+  // Hashage de mot de passe
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save() // Création de l'utilisateur
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+// Connexion
+exports.login = (req, res, next) => {
+
+
+
+  
+};
--- back/server.js
+++ back/server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Reponse du serveur');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
