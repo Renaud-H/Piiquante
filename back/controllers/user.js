3fd36afe15dc7d50e1e2e54a84063a7837c00ce9
Renaud-H <theqof@hotmail.fr>
2023-09-26 03:27:45 +0200

    préparation multer

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -44,9 +44,11 @@ exports.login = (req, res, next) => {
           //Génération du token
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, `'${process.env.TOKEN}'`, {
-              expiresIn: "24h",
-            }),
+            token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+            )
           });
         })
         .catch((error) => res.status(500).json({ error }));
---
+++ back/middleware/multer-config.js
@@ -0,0 +1,26 @@
+// Import des modules
+const multer = require('multer');
+
+// Types d'images
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+//Stockage des images
+const storage = multer.diskStorage({
+  //Dossier de stockage
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  //Renommage des fichiers
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+//Export du middleware
+module.exports = multer({storage: storage}).single('image');
